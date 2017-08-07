cfbb43c8090a097f09aedf57a167e4585287f56c
Elwin <elwindokter@gmail.com>
2017-08-07 23:26:35 +0200

    example kirby plugin to install using composer

---
+++ example.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: elwin <elwin@reprovinci.nl>
+ * Date: 8/7/17
+ * Time: 11:24 PM
+ */
+dump('this is an example/demonstration plugin that can be installed using composer');
+die;
